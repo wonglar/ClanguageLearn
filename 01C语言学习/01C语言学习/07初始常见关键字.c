c3167103c6141bb597580333c4078c8db6e886ed
wonglar <wonglar@163.com>
2022-08-15 23:49:09 +0800

    第三次提交

---
+++ 01C语言学习/01C语言学习/07初始常见关键字.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+/*
+ C语言提供的关键字
+ 1、C语言提供的，不能自己创建关键字
+ 2、变量名不能是关键字
+	
+	关键字auto，是自动的，默认的局部变量都是auto的，基本都是省略掉；每个局部变量都是auto修饰的
+
+	register 寄存器关键字，用它来创建变量是建议放到寄存器中，如 register int num = 10;
+*/
+
+int main()
+{
+
+
+	return 0;
+}
