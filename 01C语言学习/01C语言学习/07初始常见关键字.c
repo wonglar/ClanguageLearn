88397ff03e8c66d0869a783e9cc0649bfe77f72d
wonglar <wonglar@163.com>
2022-08-16 22:22:13 +0800

    关键字的使用

--- 01C语言学习/01C语言学习/07初始常见关键字.c
+++ 01C语言学习/01C语言学习/07初始常见关键字.c
@@ -11,9 +11,62 @@
 	register 寄存器关键字，用它来创建变量是建议放到寄存器中，如 register int num = 10;
 */
 
+
+// typedef 类型定义（类型重定义）
+
+//typedef unsigned int u_int;
+//int main()
+//{
+//	//unsigned int num1 = 100;  // 此代码等价于下一行代码
+//	u_int num2 = 100;
+//
+//
+//
+//	return 0;
+//}
+
+
+// 关键字static
+// 1、static修饰局部变量	static 修饰局部变量，改变了局部变量的生命周期（本质上是改变了变量的存储类型）
+// 2、static修饰全局变量
+// 3、static修饰函数
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d ", a);
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//	}
+//
+//	return 0;
+//}
+
+
+// 声明
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n", g_val);
+//
+//	return 0;
+//}
+
+
+// 声明函数
+extern int add(int x, int y);
 int main()
 {
-
-
+	int a = 10;
+	int b = 20;
+	int sum = add(a, b);
+	printf("sum = %d\n", sum);
 	return 0;
 }
--- 01C语言学习/01C语言学习/add.c
+++ 01C语言学习/01C语言学习/add.c
@@ -1,3 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-//int g_val = 2023;
+//int g_val = 2023;  // 全局变量在整个工程中都可以使用
+
+// 全局变量
+//static int g_val = 2022;  // static修饰全局变量，使得这个全局变量只能在自己所在的源文件内部可以使用。
+
+// static 修饰函数
+// static修饰函数使得函数只能在自己算在源文件内部访问，不能再其他源文件内部使用
+static int add(int x, int y)
+{
+	return x + y;
+}
