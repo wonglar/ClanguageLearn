a856fdb8752915a1ef20bd9d19f3fb037e39eb73
lar <wonglar@163.com>
2022-08-24 17:17:15 +0800

    函数

---
+++ 03C语言学习/03C语言学习/01函数.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+// ×Ö·û´®¿½±´ strcpy
+// char * strcpy ( char * destination, const char * source );
+//int main()
+//{
+//	char arr1[20] = {0};
+//	char arr2[] = "hello world";
+//
+//	strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
+
+
+
+// ÄÚ´æÉèÖÃº¯Êý memeset
+// void * memset ( void * ptr, int value, size_t num );
+
+//int main()
+//{
+//	char arr[] = "hello world";
+//	memset(arr, 'x', 5);
+//	printf("%s\n", arr);
+//
+//	return 0;
+//}
+
+
---
+++ 03C语言学习/03C语言学习/02自定义函数.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+//求两个整数的较大值
+//int get_max(int a, int b)
+//{
+//	return a > b ? a : b;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	int max = get_max(a, b);
+//	printf("max = %d\n", max);
+//	return 0;
+//}
+
+
+
+int main()
+{
+
+	return 0;
+}
